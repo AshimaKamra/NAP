85123f85903454556a83c66873df09767d8a6b26
AshimaKamra <akkamra18@gmail.com>
2020-05-31 13:59:02 +0530

    Updates

---
+++ precache-manifest.77026192329c5877fd7e2658edbee488.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45bdb9e9ba2e2b713a532e41c34b7cb",
+    "url": "/AshimaKamra/NAP/index.html"
+  },
+  {
+    "revision": "ff73167ce5779866e6b3",
+    "url": "/AshimaKamra/NAP/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "fc066168e4902c81d717",
+    "url": "/AshimaKamra/NAP/static/css/main.50fd303b.chunk.css"
+  },
+  {
+    "revision": "ff73167ce5779866e6b3",
+    "url": "/AshimaKamra/NAP/static/js/2.2f541c0f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/AshimaKamra/NAP/static/js/2.2f541c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc066168e4902c81d717",
+    "url": "/AshimaKamra/NAP/static/js/main.cf7d3842.chunk.js"
+  },
+  {
+    "revision": "ca380cadc130d2a5091a",
+    "url": "/AshimaKamra/NAP/static/js/runtime-main.d48238eb.js"
+  },
+  {
+    "revision": "899822584b1775682f5a350e37b1ea66",
+    "url": "/AshimaKamra/NAP/static/media/Banner-Background.89982258.jpg"
+  },
+  {
+    "revision": "073b815afdf5b627ed94211d9e72a634",
+    "url": "/AshimaKamra/NAP/static/media/contact-me.073b815a.jpg"
+  }
+]);
